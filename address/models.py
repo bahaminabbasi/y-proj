46457b54c5b3630f337eb08c938efd8a518c99e8
Bahamin <abbasi.bahamin@gmail.com>
2020-12-02 11:12:51 +0330

    profile and address model created

--- address/models.py
+++ address/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from userprofile.models import UserProfile
+
+
+class Address(models.Model):
+    user_name              = models.ForeignKey(UserProfile, blank=True, null=True, on_delete=models.CASCADE)
+    state                  = models.CharField(max_length=120)
+    city                   = models.CharField(max_length=120)
+    full_address           = models.CharField(max_length=120)
+    postal_code            = models.IntegerField()
+    receiver               = models.CharField(max_length=120)
+    receiver_phone         = models.CharField(max_length=120)
+
+
+    def __str__(self):
+        return f'{self.receiver} {self.city}'
--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class UserProfile(models.Model):
+    user_name           = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    first_name          = models.CharField(max_length=120, null=True, blank=True)
+    last_name           = models.CharField(max_length=120, null=True, blank=True)
+    contact_number      = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+def create_profile(sender, instance, *args, **kwargs):
+    UserProfile.objects.create(user_name=instance)
+
+post_save.connect(create_profile, sender=User)
+
