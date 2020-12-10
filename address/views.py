68b869468086d7f029718b27c3c4d994e0455566
chengu <chenguquab@gmail.com>
2020-12-10 18:47:33 +0330

    whatever

--- address/views.py
+++ address/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def add_address(request):
+    return redirect('userprofile:main-profile-page')
