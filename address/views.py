5366e63d112716d7006f5f6bc256ea01e0cdd3a1
chengu <chenguquab@gmail.com>
2020-12-20 10:44:52 +0330

    address add

--- address/views.py
+++ address/views.py
@@ -1,21 +1,36 @@
 from django.shortcuts import render, redirect
+from django.conf import settings
 
 from .models import Address
+from userprofile.models import UserProfile
 
 
 def add_address(request):
     user = request.user
     if request.method == 'POST':
-        print(request.POST)
+        user_profile = UserProfile.objects.filter(user_name=user).first()
         state = request.POST.get('state')
         city = request.POST.get('city')
         full_address = request.POST.get('full_address')
         plaque = request.POST.get('plaque')
         flat = request.POST.get('flat')
         postal_code = request.POST.get('postal_code')
-        receiver_firstname = request.POST.get('receiver_name')
+        receiver_firstname = request.POST.get('receiver_firstname')
         receiver_lastname = request.POST.get('receiver_lastname')
-        receiver_phonenumber = request.POST.get('receiver_phonenumber')
+        receiver_phone = request.POST.get('receiver_phone')
+        b = Address(
+            user_name=user_profile,
+            state=state,
+            city=city,
+            full_address=full_address,
+            plaque=plaque,
+            flat=flat,
+            postal_code=postal_code,
+            receiver_firstname=receiver_firstname,
+            receiver_lastname=receiver_lastname,
+            receiver_phone=receiver_phone
+        )
+        b.save()
         context = {
             'active_tab': 'address_tab',
         }
